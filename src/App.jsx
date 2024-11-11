32718c1c0e699c619a1c9a01bdda854997e72e8e
Nina Zyabrina <zyabrina95@gmail.com>
2024-11-11 16:41:40 +0100

    basic NavBar and react-router-dom template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import './App.css'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Dashboard from './components/Dashboard.jsx'
+import RootLayout from './components/RootLayout.jsx'
+import Cart from './components/Cart.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 import Product from './components/Product.jsx';
 
+// react-router-dom - это такая полезная штука, которая позволяет задавать пути по страницам (например, 'http://localhost:5173/cart' или 'http://localhost:5173/dashboard')
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={ <RootLayout/> }>
+      <Route index element={ <Dashboard /> }></Route>
+      <Route path="/cart" element={ <Cart /> }></Route>
+    </Route>
+  ))
   return (
-    <Product />
+    <div className='App'>
+      <RouterProvider router={router}></RouterProvider>
+      <Product />
+    </div>
   )
 }
 
